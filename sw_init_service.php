65246af23d887bbc9953a19ea06b6ab82ad0b6c1
fakharksa <fakhar@devdksa.com>
2024-06-27 03:52:28 +0500

    Added .env support using 'vlucas phpdotenv' package

--- sw_init_service.php
+++ sw_init_service.php
@@ -1,12 +1,14 @@
 <?php
     declare(strict_types=1);
-    require __DIR__ . '/vendor/autoload.php';
+    require_once realpath(__DIR__ . '/vendor/autoload.php');
     include_once __DIR__.'/helper.php';
 
     ini_set('memory_limit', -1);
     $key_dir = __DIR__. '/ssl';
 
     if (isset($argc)) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->safeLoad();
         include('service_creation_params.php');
         include('sw_service.php');
 
