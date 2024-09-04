7290af67a3d0215253931da1c392dade1dcefb69
Fakhar Anwar <fakhar.anwar@gmail.com>
2024-09-04 13:36:44 +0500

    Added array based configuration files

--- config/app_config.php
+++ config/app_config.php
@@ -1,29 +1,18 @@
 <?php
-global $swoole_ext;
+
 $swoole_ext = (extension_loaded('swoole') ? 1 : (extension_loaded('openswoole') ? 2: 0));
 if (!$swoole_ext) {
     echo "Swoole Extension Missing.".PHP_EOL." Noticed From Within File: ".__FILE__.PHP_EOL." Line: ".__LINE__.PHP_EOL;
     exit;
 }
 
-global $swoole_app_debug;
-$swoole_app_debug= (intval($_ENV['SWOOLE_APP_DEBUG']) ?? 1);
-
-global $db_connection_pool_size;
-$db_connection_pool_size = (isset($_ENV['SWOOLE_DB_CONNECTION_POOL_SIZE']) ? $_ENV['SWOOLE_DB_CONNECTION_POOL_SIZE'] : 4);
-
-global $app_type_database_driven;
-$app_type_database_driven= (intval($_ENV['SWOOLE_APP_TYPE_DATABASE_DRIVEN']) ?? 1);
-
-global $swoole_pg_db_key;
-$swoole_pg_db_key=($_ENV['SWOOLE_PG_DB_KEY'] ?? 'pg');
-
-global $swoole_mysql_db_key;
-$swoole_mysql_db_key=($_ENV['SWOOLE_MYSQL_DB_KEY'] ?? 'mysql');
-
-global $swoole_timer_time1;
-$swoole_timer_time1=(intval($_ENV['SWOOLE_TIMER_TIME1']) ?? 1000);
-
-global $swoole_daemonize;
-$swoole_daemonize=(intval($_ENV['SWOOLE_DAEMONIZE']) ?? 0);
-
+return [
+    'swoole_ext' => $swoole_ext,
+    'swoole_app_debug' => intval($_ENV['SWOOLE_APP_DEBUG']) ?? 1,
+    'db_connection_pool_size' => $_ENV['SWOOLE_DB_CONNECTION_POOL_SIZE'] ?? 4,
+    'app_type_database_driven' => intval($_ENV['SWOOLE_APP_TYPE_DATABASE_DRIVEN']) ?? 1,
+    'swoole_pg_db_key' => $_ENV['SWOOLE_PG_DB_KEY'] ?? 'pg',
+    'swoole_mysql_db_key' => $_ENV['SWOOLE_MYSQL_DB_KEY'] ?? 'mysql',
+    'swoole_timer_time1' => intval($_ENV['SWOOLE_TIMER_TIME1']) ?? 1000,
+    'swoole_daemonize' => intval($_ENV['SWOOLE_DAEMONIZE']) ?? 0,
+];
