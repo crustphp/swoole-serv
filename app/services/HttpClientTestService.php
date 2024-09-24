438afeae3b138f7bc389f5db51cbb23b17c4715b
fakharksa <fakhar@devdksa.com>
2024-09-24 19:14:26 +0500

    Updated http coroutine client service that talks to third-party API

--- app/services/HttpClientTestService.php
+++ app/services/HttpClientTestService.php
@@ -1,71 +1,132 @@
 <?php
 
+use DB\DbFacade;
 use Swoole\Coroutine\Http\Client;
 
 class HttpClientTestService
 {
     protected $webSocketServer;
     protected $frame;
+    protected $dbConnectionPools;
+    protected $postgresDbKey;
+    protected $mySqlDbKey;
 
-    public function __construct($webSocketServer, $frame)
+    protected $muasheratUserToken;
+
+    const FIELDS = 'CF_HIGH,CF_LAST,CF_LOW,CF_VOLUME,HIGH_1,HST_CLOSE,LOW_1,NETCHNG_1,NUM_MOVES,OPEN_PRC,PCTCHNG,TRDPRC_1,TURNOVER,YRHIGH,YRLOW,YR_PCTCH,CF_CLOSE,BID,ASK,ASKSIZE,BIDSIZE';
+
+    public function __construct($webSocketServer, $frame, $dbConnectionPools, $postgresDbKey = null, $mySqlDbKey = null)
     {
+        $swoole_pg_db_key = config('app_config.swoole_pg_db_key');
+        $swoole_mysql_db_key = config('app_config.swoole_mysql_db_key');
         $this->webSocketServer = $webSocketServer;
         $this->frame = $frame;
+        $this->dbConnectionPools = $dbConnectionPools;
+        $this->postgresDbKey = $postgresDbKey ?? $swoole_pg_db_key;
+        $this->mySqlDbKey = $mySqlDbKey ?? $swoole_mysql_db_key;
+
+        // Change this token ('Add Authentication Token here of any staging user')
+        $this->muasheratUserToken = $_ENV['STAGING_USER_TOKEN'];
     }
 
     public function handle()
     {
-        // Using Free API: https://fruityvice.com/doc/index.html#api-GET-getAll
-        $defaultEndpoint = "https://www.fruityvice.com/api/fruit/all";
+        // Usecase:
+        // We will fetch the company rics from the Database
+        // Than we will make chunks of 100 as Refinitiv API can fetch the maximum data of 100 rics/universe.
+        // We use execute the refinitiv API asynchronously for all the chunks and push the data to fd.
 
-        // parse_url function return various components of the url e.g host, scheme, port, path, query, fragment
-        // not necessarily returns all the components, it depends upon the url you are passing
-        // Find details of parse url function: https://www.php.net/manual/en/function.parse-url.php 
-        $parsedUrl = parse_url($defaultEndpoint);
+        $objDbPool = $this->dbConnectionPools[$this->postgresDbKey];
+        $companiesRics = new Swoole\Coroutine\Channel(1);
+        $refinitivToken = new Swoole\Coroutine\Channel(1);
 
-        // in this url we get following 3 components when parsed
-        // array:3 [
-        //     "scheme" => "https"
-        //     "host" => "www.fruityvice.com"
-        //     "path" => "/api/fruit/all"
-        // ];
+        // Fetch the companies RICs from the database
+        go(function () use ($companiesRics, $objDbPool) {
+            $dbQuery = "SELECT ric FROM companies
+                WHERE ric IS NOT NULL
+                AND ric NOT LIKE '%^%'";
 
-        $isHttps = ($parsedUrl['scheme'] === 'https');
-        $port = $isHttps ? 443 : 80; // Port 443 for https and ssl should be true. For http use port 80 with ssl equals false
+            $dbFacade = new DbFacade();
+            $rics = $dbFacade->query($dbQuery, $objDbPool);
+            $companiesRics->push(array_column($rics, 'ric'));
+        });
 
-        // Client Constructor Params: host, port, ssl
-        $client = new Client($parsedUrl['host'], $port, $isHttps);
+        // Fetch the Refinitiv AuthToken
+        go(function () use ($refinitivToken) {
+            $token = $this->getRefinitivToken();
+            $refinitivToken->push($token);
+        });
 
-        // OpenSwoole: https://openswoole.com/docs/modules/swoole-client-overall-config-set-options
-        // Swoole: https://wiki.swoole.com/en/#/client?id=configuration
+        while (true) {
+            //  We should have both Company RICs and Refinitiv Access Token
+            if (!$companiesRics->isEmpty() && !$refinitivToken->isEmpty()) {
+                $ricsChunk = array_chunk($companiesRics->pop(), 100);
+
+                $queryParams = [
+                    "fields" => self::FIELDS,
+                    "count" => 1,
+                    "interval" => "P1D",
+                    "sessions" => "normal",
+                    "adjustments" => [
+                        "exchangeCorrection",
+                        "manualCorrection",
+                        "CCH",
+                        "CRE",
+                        "RTS",
+                        "RPO"
+                    ]
+                ];
+
+                $accessToken = $refinitivToken->pop();
+
+                // We will execute each chunk in Corroutine to get data parallelly
+                foreach ($ricsChunk as $chunk) {
+                    go(function () use ($chunk, $queryParams, $accessToken) {
+                        $queryParams['universe'] = '/' . implode(',/', $chunk);
+                        $snapshots = $this->getSnapshotData($accessToken, $queryParams);
+                        $this->webSocketServer->push($this->frame->fd, $snapshots);
+                    });
+                }
+
+                break;
+            }
+        }
+    }
+
+
+    /**
+     * Get the refinitiv access token from staging
+     *
+     * @return string
+     */
+    function getRefinitivToken(): string
+    {
+        $host = 'muasherat.devdksa.com';
+        $port = 443; // This must be changed to port 80 if using http, instead of https.
+        $endpoint = '/api/get-refinitive-token';
+
+        $client = new Client($host, $port, true);
+
+        // https://openswoole.com/docs/modules/swoole-client-overall-config-set-options
         $client->set(['timeout' => 1]);
 
+        // Better form to set header
+        // Few Headers are commented as the client is fetching the data even without specifying these headers
         $headers = [
-            'Host' => $parsedUrl['host'],
+            'Host' => 'muasherat.devdksa.com',
             'Connection' => 'keep-alive\r\n',
             'Cache-Control' => 'max-age=0',
-            'Accept' => 'application/json',
+            'Accept' => 'text/html,application/xhtml+xml,application/xml, application.json;q=0.9,*/*;q=0.8',
             'User-Agent' => 'swoole-http-client',
+            'Authorization' => 'Bearer ' . $this->muasheratUserToken,
         ];
 
         $client->setHeaders($headers);
 
-        // Get call - Method 1
-        $client->get($parsedUrl['path']);
-
-        // The following code for sending get request also works
-        // Example code for this method:
-        // Swoole: https://wiki.swoole.com/en/#/coroutine_client/http_client?id=execute
-
-        // Get call - Method 2
-        // $client->setMethod('GET');
-        // $status = $client->execute($parsedUrl['path']);
-        // echo PHP_EOL.'STATUS: ';
-        // var_dump($status);
+        $client->get($endpoint);
 
         // Read Response
-        $response = $client->body;
-        var_dump($response);
+        $token_record = $client->body;
 
         echo PHP_EOL;
         echo '\n Connection Related Error Code';
@@ -81,6 +142,85 @@ class HttpClientTestService
 
         $client->close();
 
-        $this->webSocketServer->push($this->frame->fd, $response);
+        $token_record = json_decode($token_record);
+        return $token_record->access_token;
+    }
+
+    /**
+     * Get the pricing snapshots from Refinitiv
+     *
+     * @param  string $token
+     * @param  array $queryParams
+     * @return string A json encoded string of snapshot data
+     */
+    function getSnapshotData(string $token, array $queryParams): string
+    {
+        // Here I will use parse_url() function of PHP
+        $apiEndpoint = "https://api.refinitiv.com/data/pricing/snapshots/v1/";
+
+        // Add Query Params to URL
+        $apiEndpoint .= '?' . http_build_query($queryParams);
+
+        // parse_url function return various components of the url e.g host, scheme, port, path, query, fragment
+        // not necessarily returns all the components, it depends upon the url you are passing
+        // Find details of parse url function: https://www.php.net/manual/en/function.parse-url.php 
+        $parsedUrl = parse_url($apiEndpoint);
+
+        // in this url we get following 3 components when parsed
+        // array:3 [
+        //     "scheme" => "https"
+        //     "host" => "api.refinitiv.com"
+        //     "path" => "/data/pricing/snapshots/v1/"
+        //     "query" => "fields=CF_HIGH,CF_LAST,CF_LOW,CF_VOLUME,HIGH_1 ....";
+        // ];
+
+        $isHttps = ($parsedUrl['scheme'] === 'https');
+        $port = $isHttps ? 443 : 80; // Port 443 for https and ssl should be true. For http use port 80 with ssl equals false
+
+        // Client Constructor Params: host, port, ssl
+        $client = new Client($parsedUrl['host'], $port, $isHttps);
+
+        // OpenSwoole: https://openswoole.com/docs/modules/swoole-client-overall-config-set-options
+        // Swoole: https://wiki.swoole.com/en/#/client?id=configuration
+        $client->set(['timeout' => 2]);
+
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $token,
+        ];
+
+        $client->setHeaders($headers);
+
+        // Get call - Method 1
+        $client->get($apiEndpoint);
+
+        // The following code for sending get request also works
+        // Example code for this method:
+        // Swoole: https://wiki.swoole.com/en/#/coroutine_client/http_client?id=execute
+
+        // Get call - Method 2
+        // $client->setMethod('GET');
+        // $status = $client->execute($apiEndpoint);
+        // echo PHP_EOL.'STATUS: ';
+        // var_dump($status);
+
+        // Read Response
+        $response = $client->body;
+
+        echo PHP_EOL;
+        echo '\n Connection Related Error Code';
+        var_dump($client->errCode);
+
+        echo PHP_EOL;
+        echo '\nConnection Related ErrorMessage';
+        var_dump($client->errMsg);
+
+        echo PHP_EOL;
+        echo '\n Response Status Code:';
+        var_dump($client->statusCode);
+
+        $client->close();
+
+        return $response;
     }
 }
