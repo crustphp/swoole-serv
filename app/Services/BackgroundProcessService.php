5b17c0bcf167b7286996637a45be7ec6b71b8ae0
Fakhar Anwar <fakhar.anwar@gmail.com>
2024-10-08 15:19:39 +0500

    Cover cases of 'data populating' in swoole Table, when server restarts

--- app/Services/BackgroundProcessService.php
+++ app/Services/BackgroundProcessService.php
@@ -70,81 +70,155 @@ class BackgroundProcessService
                 $companyDetail[$row['ric']] = $row;
             }
 
-            swTimer::tick(config('app_config.most_active_refinitive_timespan'), function () use ($worker_id, $companyDetail, $objDbPool, $dbFacade) {
-                $service = new MostActiveRefinitive($this->server, $this->dbConnectionPools[$worker_id]);
-                $responses = $service->handle();
-                $date = Carbon::now()->format('Y-m-d H:i:s');;
-                $topGainers = [];
-                $mostActiveValues = [];
-                $doneDealCounts = [];
-                $frequentlyTradedStocks = [];
+            // Check data exist into swoole and DB table
+            $selector = new TableSelector('ref_top_gainers');
+            $cacheTopgGinersData = $selector->execute();
+            // Convert the result to an array
+            $cacheTopgGinersData = $cacheTopgGinersData->toArray();
 
+            if (count($cacheTopgGinersData) === 0) { // If swoole table is empty
+                var_dump("Swoole ref_top_gainers table is empty");
+                $dbQuery = " SELECT rtg.*, c.name AS en_long_name, c.sp_comp_id, c.short_name AS en_short_name, c.symbol, c.isin_code, c.created_at, c.arabic_name AS ar_long_name,
+                c.arabic_short_name AS ar_short_name, c.ric
+                FROM ref_top_gainers rtg
+                JOIN companies c ON rtg.company_id = c.id";
+                $dbTopgGinersData = $dbFacade->query($dbQuery, $objDbPool);
 
-                foreach ($responses as $res) {
-                    $company = isset($res['Key']["Name"]) ? $companyDetail[str_replace('/', '', $res['Key']["Name"])] : null;
+                // Check if a record exists
+                if (!empty($dbTopgGinersData)) {
+                    // Fetch the first record
+                    $latestTopGainer = $dbTopgGinersData[0];
+                    $table = SwooleTableFactory::getTable('ref_top_gainers');
 
-                    if (!empty($res) && isset($res['Fields']["TRDPRC_1"]) && isset($res['Fields']["PCTCHNG"])) { // Top gainers
+                    // Parse the latest_update timestamp
+                    $latestUpdate = Carbon::parse($latestTopGainer['latest_update']);
 
-                        $data = [
-                            'calculated_value' => $res['Fields']["PCTCHNG"],
-                            'latest_value' => $res['Fields']["TRDPRC_1"],
-                            'latest_update' => $date,
-                            'company_id' => $company['id'],
+                    // Check if the latest update is more than 5 minutes old
+                    if ($latestUpdate->diffInMinutes(Carbon::now()) >= 5) {
+                        // Record is older than 5 minutes, trigger the update process
+                        var_dump("Record is older than 5 minutes. Updating data...");
+                        $this->mostActive($worker_id, $companyDetail, $objDbPool, $dbFacade);
+                    } else {
+                        // Proceed with processing the data since it's within the last 5 minutes
+                        foreach ($dbTopgGinersData as $key => $topGainer) {
+                            $data = [
+                                'calculated_value' => $topGainer['calculated_value'],
+                                'latest_value' => $topGainer['latest_value'],
+                                'latest_update' => $topGainer['latest_update'],
+                                'company_id' => $topGainer['company_id'],
 
-                            'en_long_name' =>  $company['name'],
-                            'sp_comp_id' =>  $company['sp_comp_id'],
-                            'en_short_name' =>  $company['short_name'],
-                            'symbol' =>  $company['symbol'],
-                            'isin_code' =>  $company['isin_code'],
-                            'created_at' =>  $company['created_at'],
-                            'ar_long_name' =>  $company['arabic_name'],
-                            'ar_short_name' =>  $company['arabic_short_name'],
-                            'ric' =>  $company['ric'],
-                        ];
-                        $topGainers[] = $data;
+                                'en_long_name' =>  $topGainer['en_long_name'],
+                                'sp_comp_id' =>  $topGainer['sp_comp_id'],
+                                'en_short_name' =>  $topGainer['en_short_name'],
+                                'symbol' =>  $topGainer['symbol'],
+                                'isin_code' =>  $topGainer['isin_code'],
+                                'created_at' =>  $topGainer['created_at'],
+                                'ar_long_name' =>  $topGainer['ar_long_name'],
+                                'ar_short_name' =>  $topGainer['ar_short_name'],
+                                'ric' =>  $topGainer['ric'],
+                            ];
+
+                            var_dump("Record is within the last 5 minutes. Data prepared.");
+                            go(function () use ($key, $data, $table) {
+                                $table->set($key, $data);
+                            });
+                        }
                     }
-
-                    if (!empty($res) && isset($res['Fields']["TURNOVER"])) {  // Most Active Value
-                        $data = [
-                            'turnover' => $this->formatValue($res['Fields']["TURNOVER"]),
-                            'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
-                            'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
-                            'company_id' => $company['id'],
-                            'latest_update' => $date,
-                        ];
-                        $mostActiveValues[] = $data;
-                    }
-
-                    if (!empty($res) && isset($res['Fields']["NUM_MOVES"])) {  // Deal Done count
-                        $data = [
-                            'count' => $res['Fields']["NUM_MOVES"],
-                            'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
-                            'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
-                            'company_id' => $company['id'],
-                            'latest_update' => $date,
-
-                        ];
-                        $doneDealCounts[] = $data;
-                    }
-
-                    if (!empty($res) && isset($res['Fields']["CF_VOLUME"])) {  // Traded Stock
-                        $data = [
-                            'traded_volume' => $this->formatValue($res['Fields']["CF_VOLUME"]),
-                            'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
-                            'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
-                            'company_id' => $company['id'],
-                            'latest_update' => $date,
-                        ];
-                        $frequentlyTradedStocks[] = $data;
-                    }
+                } else { // If data not exist into database
+                    $this->mostActive($worker_id, $companyDetail, $objDbPool, $dbFacade);
                 }
+            }
 
-                $this->storeInToDBAndSwooleTable($topGainers, 'ref_top_gainers', 'calculated_value', $objDbPool, $dbFacade); // Top Gainers
-
+            // Schedule of Most Active Data Fetching
+            swTimer::tick(config('app_config.most_active_refinitive_timespan'), function () use ($worker_id, $companyDetail, $objDbPool, $dbFacade) {
+                $this->mostActive($worker_id, $companyDetail, $objDbPool, $dbFacade);
             });
         }, false, SOCK_DGRAM, true);
 
         $this->server->addProcess($refinitivBackgroundProcess);
+    }
+
+    /**
+     * Most active data processing
+     *
+     * @param  int  $worker_id
+     * @param  array  $companyDetail
+     * @param  Object  $objDbPool
+     * @param  Object  $dbFacade
+     *
+     * @return void
+     */
+
+    public function mostActive(int $worker_id, array $companyDetail, Object $objDbPool, Object $dbFacade) {
+        include_once __DIR__ . '/MostActiveRefinitive.php';
+        $service = new MostActiveRefinitive($this->server, $this->dbConnectionPools[$worker_id]);
+        $responses = $service->handle();
+        $date = Carbon::now()->format('Y-m-d H:i:s');;
+        $topGainers = [];
+        $mostActiveValues = [];
+        $doneDealCounts = [];
+        $frequentlyTradedStocks = [];
+
+        foreach ($responses as $res) {
+            $company = isset($res['Key']["Name"]) ? $companyDetail[str_replace('/', '', $res['Key']["Name"])] : null;
+
+            if (!empty($res) && isset($res['Fields']["TRDPRC_1"]) && isset($res['Fields']["PCTCHNG"])) { // Top gainers
+
+                $data = [
+                    'calculated_value' => $res['Fields']["PCTCHNG"],
+                    'latest_value' => $res['Fields']["TRDPRC_1"],
+                    'latest_update' => $date,
+                    'company_id' => $company['id'],
+
+                    'en_long_name' =>  $company['name'],
+                    'sp_comp_id' =>  $company['sp_comp_id'],
+                    'en_short_name' =>  $company['short_name'],
+                    'symbol' =>  $company['symbol'],
+                    'isin_code' =>  $company['isin_code'],
+                    'created_at' =>  $company['created_at'],
+                    'ar_long_name' =>  $company['arabic_name'],
+                    'ar_short_name' =>  $company['arabic_short_name'],
+                    'ric' =>  $company['ric'],
+                ];
+                $topGainers[] = $data;
+            }
+
+            if (!empty($res) && isset($res['Fields']["TURNOVER"])) {  // Most Active Value
+                $data = [
+                    'turnover' => $this->formatValue($res['Fields']["TURNOVER"]),
+                    'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
+                    'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
+                    'company_id' => $company['id'],
+                    'latest_update' => $date,
+                ];
+                $mostActiveValues[] = $data;
+            }
+
+            if (!empty($res) && isset($res['Fields']["NUM_MOVES"])) {  // Deal Done count
+                $data = [
+                    'count' => $res['Fields']["NUM_MOVES"],
+                    'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
+                    'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
+                    'company_id' => $company['id'],
+                    'latest_update' => $date,
+
+                ];
+                $doneDealCounts[] = $data;
+            }
+
+            if (!empty($res) && isset($res['Fields']["CF_VOLUME"])) {  // Traded Stock
+                $data = [
+                    'traded_volume' => $this->formatValue($res['Fields']["CF_VOLUME"]),
+                    'latest_value' => isset($res['Fields']["TRDPRC_1"]) ? $res['Fields']["TRDPRC_1"] : null,
+                    'calculated_value' => isset($res['Fields']["PCTCHNG"]) ?  $res['Fields']["PCTCHNG"] : null,
+                    'company_id' => $company['id'],
+                    'latest_update' => $date,
+                ];
+                $frequentlyTradedStocks[] = $data;
+            }
+        }
+
+        $this->storeInToDBAndSwooleTable($topGainers, 'ref_top_gainers', 'calculated_value', $objDbPool, $dbFacade); // Top Gainers
     }
 
     /**
