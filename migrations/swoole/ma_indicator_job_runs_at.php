84715ab554423365b9cda4fd672cf0a95b047251
Fakhar Anwar <fakhar.anwar@gmail.com>
2024-10-24 21:23:20 +0500

    rename job_runs.php as ma_indicator_job_runs_at

--- migrations/swoole/ma_indicator_job_runs_at.php
+++ migrations/swoole/ma_indicator_job_runs_at.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'table_name' => 'job_runs', // Proper name indicating it's for storing job run timestamps
+    'table_name' => 'ma_indicator_job_runs_at', // Proper name indicating it's for storing job run timestamps
     'table_size' => 1024, // Size can be smaller depending on how many job runs you're tracking
     'columns' => [
         ['name' => 'job_run_at', 'type' => 'string', 'size' => 128]
