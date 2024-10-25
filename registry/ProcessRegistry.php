04e0c78adefacb4c22393fc4c83ccc0326bec0d3
fakharksa <fakhar@devdksa.com>
2024-10-25 12:23:44 +0500

    Registration of example custom Process in new separated process-registry array

--- registry/ProcessRegistry.php
+++ registry/ProcessRegistry.php
@@ -15,6 +15,21 @@ $registeredProcesses = [
             'enable_coroutine' => true,
         ]
     ],
+    'ref_process' => [
+        // Callback of Process you want to call when the process will be created
+        'callback' => [\App\Services\RefService::class, 'handle'],
+
+        // Optional: Array of constructor params to be used when creating the class
+        'constructor_params' => [self::$server, self::$process],
+
+        // Process Options
+        'process_options' => [
+            'redirect_stdin_and_stdout' => false,
+            'pipe_type' => SOCK_DGRAM,
+            'enable_coroutine' => true,
+        ],
+
+    ],
 
     // Add More Processes Here
 ];
