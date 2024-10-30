0f1e21a7a8bb8df5a6dbd22d6e05c33e9b9d2664
fakharksa <fakhar@devdksa.com>
2024-10-30 07:13:16 +0500

    Decoupled API-consumer class from Business Logic class into Infrastructure Layer

--- app/Services/RefAPIConsumer.php
+++ app/Services/RefAPIConsumer.php
@@ -3,8 +3,8 @@
 namespace App\Services;
 
 use Swoole\Coroutine\Barrier;
-use Swoole\Coroutine\Http\Client;
 use Swoole\Coroutine;
+use App\Core\Services\APIConsumer;
 
 class RefAPIConsumer
 {
@@ -17,20 +17,23 @@ class RefAPIConsumer
     protected $authCounter = 0;
     protected $tooManyRequestCounter = 0;
     protected $retry;
+    protected $url;
+    protected $muachratTokenUrl;
+    protected $timeout;
 
-    const FIELDS = 'CF_VOLUME,NUM_MOVES,PCTCHNG,TRDPRC_1,TURNOVER';
-
-    public function __construct($webSocketServer, $dbConnectionPools, $dbFacade)
+    public function __construct($webSocketServer, $dbConnectionPools, $dbFacade, $url, $token)
     {
         $this->webSocketServer = $webSocketServer;
         $this->dbConnectionPools = $dbConnectionPools;
         $this->dbFacade = $dbFacade;
-        // Change this token ('Add Authentication Token here of any staging user')
-        $this->muasheratUserToken = $_ENV['STAGING_USER_TOKEN'];
-        $this->retry =  config('app_config.refinitv_retry');
+        $this->muasheratUserToken = $token;
+        $this->retry = config('app_config.refinitv_retry');
+        $this->url = $url;
+        $this->muachratTokenUrl = config('app_config.app_url').'/api/get-refinitive-token';
+        $this->timeout = config('app_config.refinitiv_req_timeout');
     }
 
-    public function handle($companies = null)
+    public function handle($companies = null, $fields)
     {
         if ($companies) {
             $companiesRics = array_column($companies, 'ric');
@@ -54,16 +57,12 @@ class RefAPIConsumer
         // Fetch Refinitive access token
         $refAccessToken = $this->getRefToken();
 
-        $refAccessToken = json_decode($refAccessToken);
-        // Check if access_token is set in the refAccessToken object
-        $refAccessToken = isset($refAccessToken->access_token) ? $refAccessToken->access_token : "";
-
         if (!empty($companiesRics) && !empty($refAccessToken)) {
             $ricsChunks =  array_chunk($companiesRics, $this->chunkSize);
 
             // Proceed if both RICs and token are available
             $queryParams = [
-                "fields" => self::FIELDS,
+                "fields" => $fields,
                 "count" => 1,
                 "interval" => "P1D",
                 "sessions" => "normal",
@@ -94,8 +93,6 @@ class RefAPIConsumer
         throw new \RuntimeException('Failed to retrieve data of most active.');
     }
 
-
-
     /**
      * Get the refinitiv access token from staging
      *
@@ -103,50 +100,13 @@ class RefAPIConsumer
      */
     function getRefToken(): string
     {
-        $host =  config('app_config.app_url');
-        $port = 443; // This must be changed to port 80 if using http, instead of https.
-        $endpoint = '/api/get-refinitive-token';
+        $headers = $this->getHeaders($this->muasheratUserToken);
 
-        $client = new Client($host, $port, true);
+        $apiConsumer = new APIConsumer($this->muachratTokenUrl, $headers, $this->timeout);
+        $response = $apiConsumer->request();
+        $data = json_decode($response['response'], true);
 
-        // https://openswoole.com/docs/modules/swoole-client-overall-config-set-options
-        $client->set(['timeout' => config('app_config.refinitiv_req_timeout')]);
-
-        // Better form to set header
-        // Few Headers are commented as the client is fetching the data even without specifying these headers
-        $headers = [
-            'Host' => config('app_config.app_url'),
-            'Connection' => 'keep-alive\r\n',
-            'Cache-Control' => 'max-age=0',
-            'Accept' => 'text/html,application/xhtml+xml,application/xml, application.json;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'swoole-http-client',
-            'Authorization' => 'Bearer ' . $this->muasheratUserToken,
-        ];
-
-        $client->setHeaders($headers);
-
-        $client->get($endpoint);
-
-        // Read Response
-        $token_record = $client->body;
-
-        if ($client->statusCode != 200) {
-            echo PHP_EOL;
-            echo '\n Connection Related Error Code';
-            var_dump($client->errCode);
-
-            echo PHP_EOL;
-            echo '\nConnection Related ErrorMessage';
-            var_dump($client->errMsg);
-
-            echo PHP_EOL;
-            echo '\n Response Status Code:';
-            var_dump($client->statusCode);
-        }
-
-        $client->close();
-
-       return $token_record;
+        return $data['access_token'] ?? '';
     }
 
     /**
@@ -158,81 +118,12 @@ class RefAPIConsumer
      */
     function getIndicatorsData(string $token, array $queryParams): array
     {
-        // var_dump('getMostActiveData function start');
-        // Here I will use parse_url() function of PHP
-        $apiEndpoint = "https://api.refinitiv.com/data/pricing/snapshots/v1/";
+        $headers = $this->getHeaders($token);
+        $endpoint = $this->url;
+        $endpoint .= '?' . http_build_query($queryParams);
+        $apiConsumer = new APIConsumer($endpoint, $headers, $this->timeout);
 
-        // Add Query Params to URL
-        $apiEndpoint .= '?' . http_build_query($queryParams);
-
-        // parse_url function return various components of the url e.g host, scheme, port, path, query, fragment
-        // not necessarily returns all the components, it depends upon the url you are passing
-        // Find details of parse url function: https://www.php.net/manual/en/function.parse-url.php
-        $parsedUrl = parse_url($apiEndpoint);
-
-        // in this url we get following 3 components when parsed
-        // array:3 [
-        //     "scheme" => "https"
-        //     "host" => "api.refinitiv.com"
-        //     "path" => "/data/pricing/snapshots/v1/"
-        //     "query" => "fields=CF_VOLUME,NUM_MOVES,PCTCHNG,TRDPRC_1,TURNOVER ....";
-        // ];
-
-        $isHttps = ($parsedUrl['scheme'] === 'https');
-        $port = $isHttps ? 443 : 80; // Port 443 for https and ssl should be true. For http use port 80 with ssl equals false
-
-        // Client Constructor Params: host, port, ssl
-        $client = new Client($parsedUrl['host'], $port, $isHttps);
-
-        // OpenSwoole: https://openswoole.com/docs/modules/swoole-client-overall-config-set-options
-        // Swoole: https://wiki.swoole.com/en/#/client?id=configuration
-        $client->set(['timeout' => config('app_config.refinitiv_req_timeout')]);
-
-
-        $headers = [
-            'Authorization' => 'Bearer ' . $token,
-        ];
-
-        $client->setHeaders($headers);
-
-        // Get call - Method 1
-        $client->get($apiEndpoint);
-
-        // The following code for sending get request also works
-        // Example code for this method:
-        // Swoole: https://wiki.swoole.com/en/#/coroutine_client/http_client?id=execute
-
-        // Get call - Method 2
-        // $client->setMethod('GET');
-        // $status = $client->execute($apiEndpoint);
-        // echo PHP_EOL.'STATUS: ';
-        // var_dump($status);
-
-        // Read Response
-        $response = $client->body;
-
-        if ($client->statusCode != 200) {
-            echo PHP_EOL;
-            echo '\n Connection Related Error Code';
-            var_dump($client->errCode);
-
-            echo PHP_EOL;
-            echo '\nConnection Related ErrorMessage';
-            var_dump($client->errMsg);
-
-            echo PHP_EOL;
-            echo '\n Response Status Code:';
-            var_dump($client->statusCode);
-        }
-
-        $statusCode = $client->statusCode;
-
-        $client->close();
-
-        return [
-            'status_code' => $statusCode,
-            'response' => $response,
-        ];
+        return $apiConsumer->request();
     }
 
     public function sendRequest($chunk, $queryParams, $accessToken, $mAIndicatorBarrier)
@@ -270,6 +161,16 @@ class RefAPIConsumer
         });
     }
 
+    function getHeaders($token)
+    {
+        return [
+            'Connection' => 'keep-alive',
+            'Cache-Control' => 'no-cache',
+            'Accept' => 'application/json,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'User-Agent' => 'swoole-http-client',
+            'Authorization' => 'Bearer ' . $token,
+        ];
+    }
 
     public function __destruct() {}
 }
