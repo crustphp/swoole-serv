45537a41b326ee9e0582341df20e156c907fddd7
Fakhar Anwar <fakhar.anwar@gmail.com>
2024-12-14 19:44:28 +0500

    Ability to use domain instead of IP in CLI & minor fixes

--- websocketclient/websocketclient_usage.php
+++ websocketclient/websocketclient_usage.php
@@ -2,17 +2,35 @@
 
 namespace Websocketclient;
 
-include_once __DIR__ . '/../includes/Autoload.php';
-
-
 use Websocketclient\WebSocketClient;
 
+$baseDir = dirname(__DIR__);
+
+require_once realpath($baseDir . '/vendor/autoload.php');
+include_once realpath($baseDir . '/includes/Autoload.php'); // Our custom Autoloader
+include_once realpath($baseDir . '/helper.php');
+
+// Setting up Env for using $_ENV variable
+require_once realpath($baseDir . '/includes/LoadEnv.php');
+
+
 $ip = '127.0.0.1';
-if (isset($argv[1]) && in_array($argv[1], ['remote'])) { // Set Default IP
-    $ip = '45.76.35.99';
+if (isset($argv[1])) { // Set Default IP
+    $argv1 = strtolower($argv[1]);
+
+    if (filter_var($argv1, FILTER_VALIDATE_IP) || preg_match('/^(?!\-)(?:[a-z0-9\-]{1,63}\.)+[a-z]{2,}$/', $argv1)) {
+        $ip = $argv1;
+    }
 }
 
-$w = new WebSocketClient($ip, 9501);
+// Default port 9501
+$port = '9501';
+if (isset($argv[2]) &&
+    preg_match('/^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/', $argv[2])) {
+   $port = $argv[2];
+}
+
+$w = new WebSocketClient($ip, $port);
 if ($x = $w->connect()) {
 //    var_dump($x);
     //reload-code
@@ -23,7 +41,7 @@ if ($x = $w->connect()) {
         }
         echo PHP_EOL."sending ".$cmd.PHP_EOL;
         $w->send($cmd, 'text', 1);
-        exit;
+        // exit;
     }
 
     for ($i=1;$i<4;$i++)
