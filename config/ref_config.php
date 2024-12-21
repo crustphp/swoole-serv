1b51825755f602c27f4cef15084e9a84b604fa64
Fakhar Anwar <fakhar.anwar@gmail.com>
2024-12-21 17:38:54 +0500

    changes in config defaults

--- config/ref_config.php
+++ config/ref_config.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'url' => $_ENV['REFINITIVE_URL'] ?? 'https://api.refinitiv.com/auth/oauth2/v1/token',
-    'username' => $_ENV['REFINITIVE_USERNAME'] ?? 'GE-A-10171559-3-15155',
-    'password' => $_ENV['REFINITIVE_PASSWORD'] ?? 'MuaIbrahim@MarketApp1010777789',
-    'grant_type' => $_ENV['REFINITIVE_GRANT_TYPE'] ?? 'password',
-    'scope' => $_ENV['REFINITIVE_SCOPE'] ?? 'trapi',
-    'take_exclusive_sign_on_control' => $_ENV['REFINITIVE_TAKE_EXCLUSIVE_SIGN_ON_CONTROL'] ?? 'true',
-    'client_id' => $_ENV['REFINITIVE_CLIENT_ID'] ?? '07f4cbaa345f49149f5afb74f3b497daa53dd4d8',
-    'refresh_grant_type' => $_ENV['REFINITIVE_REFRESH_GRANT_TYPE'] ?? 'refresh_token',
+    'url' => $_ENV['REFINITIVE_URL'] ?? '',
+    'username' => $_ENV['REFINITIVE_USERNAME'] ?? '',
+    'password' => $_ENV['REFINITIVE_PASSWORD'] ?? '',
+    'grant_type' => $_ENV['REFINITIVE_GRANT_TYPE'] ?? '',
+    'scope' => $_ENV['REFINITIVE_SCOPE'] ?? '',
+    'take_exclusive_sign_on_control' => $_ENV['REFINITIVE_TAKE_EXCLUSIVE_SIGN_ON_CONTROL'] ?? '',
+    'client_id' => $_ENV['REFINITIVE_CLIENT_ID'] ?? '',
+    'refresh_grant_type' => $_ENV['REFINITIVE_REFRESH_GRANT_TYPE'] ?? '',
 
-    'search_light_url' => $_ENV['SEARCH_LIGHT_URL'] ?? 'https://api.refinitiv.com/discovery/searchlight/v1/',
-    'historical_pricing_intraday_url' =>  $_ENV['HISTORICAL_PRICING_INTRADAY_URL'] ?? 'https://api.refinitiv.com/data/historical-pricing/v1/views/intraday-summaries/',
-    'historical_pricing_interday_url' =>  $_ENV['HISTORICAL_PRICING_INTERDAY_URL'] ?? 'https://api.refinitiv.com/data/historical-pricing/v1/views/interday-summaries/',
-    'pricing_snapshots_url' => $_ENV['PRICING_SNAPSHOTS_URL'] ?? 'https://api.refinitiv.com/data/pricing/snapshots/v1/',
+    'search_light_url' => $_ENV['SEARCH_LIGHT_URL'] ?? '',
+    'historical_pricing_intraday_url' =>  $_ENV['HISTORICAL_PRICING_INTRADAY_URL'] ?? '',
+    'historical_pricing_interday_url' =>  $_ENV['HISTORICAL_PRICING_INTERDAY_URL'] ?? '',
+    'pricing_snapshots_url' => $_ENV['PRICING_SNAPSHOTS_URL'] ?? '',
 
     'time_to_refresh_token' => $_ENV['TIME_TO_REFRESH_TOKEN'] ?? 10,
 
