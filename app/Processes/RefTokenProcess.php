b6447a0ca037e377f73c45c361ee881554039d32
Fakhar Anwar <fakhar.anwar@gmail.com>
2025-02-04 09:31:05 +0500

    Moved db pool creation code from TPTokenRetrievalAndSynchService to RefTokenProcess class

--- app/Processes/RefTokenProcess.php
+++ app/Processes/RefTokenProcess.php
@@ -3,24 +3,90 @@
 namespace App\Processes;
 
 use App\Services\TPTokenRetrievalAndSynchService;
+use DB\DBConnectionPool;
 
 class RefTokenProcess {
 
 	protected $server;
     protected $process;
 
+    protected $dbConnectionPools = [];
+    protected $poolKey;
+    protected $worker_id;
+    protected $objDbPool;
+
     public function __construct($server, $process)
     {
         $this->server = $server;
         $this->process = $process;
+
+        // Create the DB Connection Pool
+        $this->worker_id = $process->id;
+
+        $app_type_database_driven = config('app_config.app_type_database_driven');
+        if ($app_type_database_driven) {
+            $this->poolKey = makePoolKey($this->worker_id, 'postgres');
+
+            try {
+                // initialize an object for 'DB Connections Pool'; global only within scope of a Worker Process
+                // Important: make sure you use the same identifier as pool_key that you use for $this->dbConnectionPools
+                // If you want to use different identifier than modify the __destruct code accordingly
+                $this->dbConnectionPools[$this->poolKey] = new DBConnectionPool($this->poolKey, 'postgres', 'swoole', true);
+                $this->dbConnectionPools[$this->poolKey]->create();
+            } catch (\Throwable $e) {
+                echo $e->getMessage() . PHP_EOL;
+                echo $e->getFile() . PHP_EOL;
+                echo $e->getLine() . PHP_EOL;
+                echo $e->getCode() . PHP_EOL;
+                var_dump($e->getTrace());
+            }
+        }
+
+        $this->objDbPool = $this->dbConnectionPools[$this->poolKey];
     }
 
     public function handle()
     {
-        $refTokenService = new TPTokenRetrievalAndSynchService($this->server, $this->process);
-        $refTokenService->handle();
+        $tPTokenRetrievalAndSynchService = new TPTokenRetrievalAndSynchService(server: $this->server, process: $this->process, objDbPool: $this->objDbPool);
+        $tPTokenRetrievalAndSynchService->handle();
 
-        // Unset the RefTokenService
-        unset($refTokenService);
+        // Unset the TPTokenRetrievalAndSynchService
+        unset($tPTokenRetrievalAndSynchService);
+    }
+
+     /**
+     * __destruct
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        // Revoking the Database related resources
+        $app_type_database_driven = config('app_config.app_type_database_driven');
+
+        if ($app_type_database_driven) {
+            if (isset($this->dbConnectionPools)) {
+                foreach ($this->dbConnectionPools as $key => $dbConnectionPool) {
+                    echo PHP_EOL . "Closing Connection Pool: $key" . PHP_EOL;
+                    $dbConnectionPool->closeConnectionPool($key);
+                    unset($dbConnectionPool);
+                }
+
+                unset($this->dbConnectionPools);
+            }
+        }
+
+        unset($this->objDbPool);
+    }
+
+    /**
+     * Revoke the process resources
+     *
+     * @return void
+     */
+    public function revokeProcessResources() {
+        if (method_exists($this, '__destruct')) {
+            $this->__destruct();
+        }
     }
 }
